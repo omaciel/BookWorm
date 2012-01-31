abf89833f961b10e47f9c300e38bea4000f77898
Og B. Maciel <ogmaciel@gnome.org>
2012-01-31 17:54:14 -0500

    Changed layout to a single column from 2 columns and fixed tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -26,12 +26,12 @@ describe UsersController do
 
         it "should include the user's name" do
             get :show, :id => @user
-            response.should have_selector("h3", :content => @user.name)
+            response.should have_selector("h1", :content => @user.name)
         end
 
         it "should have a profile image" do
             get :show, :id => @user
-            response.should have_selector("h3>img", :class => "gravatar")
+            response.should have_selector("h1>img", :class => "gravatar")
         end
 
     end
