f4c5d3171668f2e347a7e0a4590daf992c1cfbd7
Og B. Maciel <ogmaciel@gnome.org>
2012-02-07 22:36:13 -0500

    Fixed remaining broken tests.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -54,7 +54,7 @@ describe SessionsController do
 
             it "should redirect to the user show page" do
                 post :create, :session => @attr
-                response.should redirect_to(user_path(@user))
+                response.should redirect_to(root_path)
             end
         end
 
